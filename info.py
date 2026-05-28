b0b55727e0cde9b1dcd1f66bd2f9fe569c7c09ca
Mageshdamachi <mageshmanish04@gmail.com>
2026-05-28 19:12:03 +0530

    Restore credit and bot information comments

--- info.py
+++ info.py
@@ -21,8 +21,8 @@ PICS = (environ.get('PICS', 'https://telegra.ph/file/424097f63a0e006e8f3b0.jpg')
 
 
 # Admins & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '2123077350').split()] # For Multiple Id Use One Space Between Each.
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '2123077350').split()]  # For Multiple Id Use One Space Between Each.
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5097939801').split()] # For Multiple Id Use One Space Between Each.
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '5097939801').split()]  # For Multiple Id Use One Space Between Each.
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 
 # This Channel Is For When User Start Your Bot Then Bot Send That User Name And Id In This Log Channel, Same For Group Also.
